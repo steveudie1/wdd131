51fbd8eb069fba1fe0c97ff5401d85390b7f7887
steveudie1/wdd130 <stephenudie878@gmail.com>
2026-07-11 09:06:20 +0100

    update

---
+++ week02/scripts/bom.js
@@ -0,0 +1,17 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+const li = document.createElement('li');
+
+const deleteButton = document.createElement('button');
+
+<button arial-label="close" id="close-botton"></button>
+
+buttonElement.addEventListener('click', function() {
+  // Code to execute when the element is clicked
+});
+
+button.addEventListener('click', function() {
+  // Code to execute when the button is clicked
+});
