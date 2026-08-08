f77b25114244ba3d90cb6f4e5a1846c3eab147fd
steveudie1 <stephenudie878@gmail.com>
2026-08-08 15:01:32 +0100

    finis

---
+++ project/scripts/form.js
@@ -0,0 +1,53 @@
+// Array of product objects required for the review form
+const products = [
+    { id: "fc-1888", name: "flux capacitor", averagerating: 4.5 },
+    { id: "fc-2050", name: "power laces", averagerating: 4.7 },
+    { id: "fs-1987", name: "time circuits", averagerating: 3.5 },
+    { id: "ac-2000", name: "low voltage reactor", averagerating: 3.9 },
+    { id: "jj-1969", name: "warp equalizer", averagerating: 5.0 }
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const productSelect = document.getElementById("product-select");
+
+    if (productSelect) {
+        products.forEach(product => {
+            const option = document.createElement("option");
+            option.value = product.id;
+            option.textContent = product.name;
+            productSelect.appendChild(option);
+        });
+    }
+
+    
+    const countDisplay = document.getElementById("reviewCount");
+
+    if (countDisplay) {
+        const storageKey = "numReviewsCompleted";
+        
+        
+        let reviewCount = Number(localStorage.getItem(storageKey)) || 0;
+        
+        
+        reviewCount++;
+        
+        
+        localStorage.setItem(storageKey, reviewCount);
+        
+        
+        countDisplay.textContent = reviewCount;
+    }
+
+    
+    const yearElem = document.getElementById("currentYear");
+    const modElem = document.getElementById("lastModified");
+
+    if (yearElem) {
+        yearElem.textContent = new Date().getFullYear();
+    }
+    
+    if (modElem) {
+        modElem.textContent = `Last Modification: ${document.lastModified}`;
+    }
+});
